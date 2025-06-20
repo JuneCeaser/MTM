e699a2dffb9ec38eaa56d610c4aab07c06f0ca14
June Ceaser <junecdsjune@gmail.com>
2025-06-20 10:38:49 +0530

    navigating test

--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -1,10 +1,17 @@
-import { View, Text } from 'react-native'
+import { View, Text,Button } from 'react-native'
 import React from 'react'
+import Signup from './Signup'
 
-export default function Login() {
+export default function Login({navigation}) {
   return (
     <View>
       <Text>Login</Text>
+      <Button
+      title="Go to Singup page"
+      onPress={() =>
+        navigation.navigate(Signup)
+      }
+    />
     </View>
   )
 }
