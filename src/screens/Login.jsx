b5845ce055158268e009cfbb46aa91435d323a6a
June Ceaser <junecdsjune@gmail.com>
2025-06-20 11:04:22 +0530

    login and signup create styls variable

--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -1,17 +1,28 @@
-import { View, Text,Button } from 'react-native'
+import { View, Text,Button,StyleSheet } from 'react-native'
 import React from 'react'
 import Signup from './Signup'
 
 export default function Login({navigation}) {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Login</Text>
       <Button
       title="Go to Singup page"
       onPress={() =>
         navigation.navigate(Signup)
       }
+     
     />
     </View>
   )
-}
+
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- src/screens/Signup.jsx
+++ src/screens/Signup.jsx
@@ -1,10 +1,22 @@
-import { View, Text } from 'react-native'
+import { View, Text,StyleSheet } from 'react-native'
 import React from 'react'
 
 export default function Signup() {
   return (
-    <View>
+    <View style={styles.container}> 
       <Text>Signup</Text>
     </View>
   )
-}
+};
+
+const styles = StyleSheet.create ({
+
+ container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+
+});
