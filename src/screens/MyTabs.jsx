c5524819a40347a48890ad1aed2b2233e7c47ec3
June Ceaser <junecdsjune@gmail.com>
2025-06-29 10:53:11 +0530

    tab bar styling

--- src/screens/MyTabs.jsx
+++ src/screens/MyTabs.jsx
@@ -3,18 +3,29 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import HomeScreen from "./HomeScreen";
 import ProfileScreen from "./ProfileScreen";
 import AddScreen from "./AddScreen";
+import { Ionicons } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 
 export default function MyTabs(navigation) {
   return (
     <Tab.Navigator
-      screenOptions={{
-        tabBarLabelStyle: {
-          fontSize: 12,
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === "Home") {
+            iconName = focused ? "home" : "home";
+          } else if (route.name === "Add") {
+            iconName = focused ? "add-circle-sharp" : "add-circle-sharp";
+          } else if (route.name === "Profile") {
+            iconName = "person-sharp";
+          }
+          return <Ionicons name={iconName} size={size} color={color} />;
         },
         tabBarActiveTintColor: "#3F559C",
-      }}
+        tabBarInactiveTintColor: "gray",
+      })}
     >
       <Tab.Screen
         options={{ headerShown: false }}
