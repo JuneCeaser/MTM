975fb4b2e64df6e328a161401d2176832959bbc9
June Ceaser <junecdsjune@gmail.com>
2025-07-10 12:26:41 +0530

    search filter

--- src/screens/SearchScreen.jsx
+++ src/screens/SearchScreen.jsx
@@ -1,13 +1,45 @@
-import { View, Text, SafeAreaView, StyleSheet } from "react-native";
-import React from "react";
+import { View, Text, SafeAreaView, StyleSheet, FlatList } from "react-native";
+import Task from "../components/Task";
+import React, { Component, useEffect, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-export default function SearchScreen({ route }) {
+export default function SearchScreen({ route, navigation }) {
   const { searchID } = route.params;
+  const [tasks, setTasks] = useState([]);
+  const loadTasks = async () => {
+    try {
+      const jsonValue = await AsyncStorage.getItem("tasksList2");
+      let tasks = jsonValue != null ? JSON.parse(jsonValue) : [];
+
+      tasks = tasks.filter((task) => task.title === searchID);
+
+      setTasks(tasks);
+    } catch (e) {
+      // error reading value
+    }
+  };
+
+  useEffect(() => {
+    loadTasks();
+  }, [searchID]);
 
   return (
-    <SafeAreaView style={styles.container}>
-      <Text>{searchID}</Text>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <FlatList
+        data={tasks}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <Task
+            navigation={navigation}
+            id={item.id}
+            title={item.title}
+            description={item.description}
+            date={item.dueDate}
+            status={item.status}
+          />
+        )}
+      />
+    </View>
   );
 }
 
@@ -15,7 +47,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    justifyContent: "center",
-    alignItems: "center",
   },
 });
